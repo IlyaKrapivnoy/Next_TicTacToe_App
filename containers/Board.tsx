49cf091bd3ac99a243edfbc418c0329b987baa0f
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-16 23:59:05 +0200

    add squares

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
+import Square from "../components/Square";
 
 const Board = () => {
   const [squares, getSquares] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
     Math.round(Math.random() * 1) === 1 ? "X" : "O"
   );
+  const [winner, setWinner] = useState(null);
   return (
     <>
       <div>Board is here</div>
       {currentPlayer}
+      <div className={"row"}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className={"row"}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className={"row"}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 };
