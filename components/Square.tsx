d168a046b5affb25c760b0eb4b4d03ce51dd9d61
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-16 23:56:22 +0200

    add currentPlayer

---
+++ components/Square.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Square = () => {
+    return (
+        <button>
+            Square is here
+        </button>
+    );
+};
+
+export default Square;
--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Board = () => {
-    return (
-        <div>
-            Board is here
-        </div>
-    );
+  const [squares, getSquares] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
+    Math.round(Math.random() * 1) === 1 ? "X" : "O"
+  );
+  return (
+    <>
+      <div>Board is here</div>
+      {currentPlayer}
+    </>
+  );
 };
 
 export default Board;
