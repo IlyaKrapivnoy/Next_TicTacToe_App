20bdaaa99b21659761906273c098e4b2cbb1e7db
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-17 00:09:33 +0200

    setSquareValue

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -2,29 +2,40 @@ import React, { useState } from "react";
 import Square from "../components/Square";
 
 const Board = () => {
-  const [squares, getSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
     Math.round(Math.random() * 1) === 1 ? "X" : "O"
   );
   const [winner, setWinner] = useState(null);
+
+  function setSquareValue(index: number) {
+    const newData = squares.map((val, i) => {
+      if (i === index) {
+        return currentPlayer;
+      }
+      return val;
+    });
+    setSquares(newData);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  }
+
   return (
     <>
       <div>Board is here</div>
       {currentPlayer}
-      <div className={"row"}>
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className={"row"}>
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className={"row"}>
-        <Square />
-        <Square />
-        <Square />
+      <div className="grid">
+        {Array(9)
+          .fill(null)
+          .map((_, i) => {
+            return (
+              <Square
+                winner={winner}
+                key={i}
+                onClick={() => setSquareValue(i)}
+                value={squares[i]}
+              />
+            );
+          })}
       </div>
     </>
   );
